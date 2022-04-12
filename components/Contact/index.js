affa6d3627c934d2a554324925cd3f05965b91e3
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-04-12 18:27:13 +0200

    Make google map iframe load with useEffect

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styles from './contact.module.scss';
 
 export const Contact = (props) => {
+
+    const map = React.createRef();
+
+    useEffect(() => {
+        map.current.setAttribute("src",map.current.getAttribute("data-src"));
+    }, [])
+
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
@@ -9,7 +16,7 @@ export const Contact = (props) => {
                     <li><i className="fa-solid fa-phone"/>{props.tel}</li>
                     <li><i className="fa-solid fa-map-pin"/>{props.address}</li>
                 </ul>
-                <iframe width="600" className={styles.map} height="450" loading="lazy" allowFullScreen referrerPolicy="no-referrer-when-downgrade" src={"https://maps.google.com/maps?q=" + props.address + "&t=&z=13&ie=UTF8&iwloc=&output=embed"}></iframe>
+                <iframe ref={map} width="600" className={styles.map} height="450" loading="lazy" allowFullScreen referrerPolicy="no-referrer-when-downgrade" src="" data-src={"https://maps.google.com/maps?q=" + props.address + "&t=&z=13&ie=UTF8&iwloc=&output=embed"}></iframe>
             </div>
         </div>
     )
